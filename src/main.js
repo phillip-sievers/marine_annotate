3c39063506451a87f43b8ae669b88586353241fc
Phillip Sievers <phillip.sievers@gmx.de>
2025-09-11 00:05:15 -0400

    [#7] fetch images through remote source

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const fileInput = document.getElementById("fileInput");
 const previewSection = document.getElementById("previewSection");
 const imagePreview = document.getElementById("imagePreview");
 const annotateButton = document.querySelector(".annotate-button");
+const getImageButton = document.querySelector(".get-image");
 
 uploadArea.addEventListener("click", () => fileInput.click());
 
@@ -20,3 +21,38 @@ fileInput.addEventListener("change", (e) => {
 annotateButton.addEventListener("click", () => {
     window.location.href = `annotate.html`;
 });
+
+const API_KEY = "RB6xVqFugJmNed57P07Ql_r9l45zBtMUtqA3yr4-6eo";
+
+const marineAnimals = [
+    "whale",
+    "dolphin",
+    "shark",
+    "sea turtle",
+    "octopus",
+    "jellyfish",
+    "seahorse",
+    "starfish",
+    "coral reef",
+    "tropical fish",
+];
+
+async function getRandomMarineImage() {
+    const randomAnimal =
+        marineAnimals[Math.floor(Math.random() * marineAnimals.length)];
+
+    const response = await fetch(
+        `https://api.unsplash.com/search/photos?query=${randomAnimal}&client_id=${API_KEY}&per_page=1`
+    );
+    const data = await response.json();
+
+    let src = data.results[0].urls.regular;
+    localStorage.setItem("selectedImage", src);
+    imagePreview.src = src;
+    previewSection.classList.remove("hidden");
+    uploadArea.classList.add("hidden");
+    getImageButton.disabled = true;
+    getImageButton.classList.add("hidden");
+}
+
+getImageButton.addEventListener("click", getRandomMarineImage);
